5d227cda251effaecf17d8d6cf2920b22341ffc9
Aswin Sivaraman <asivara@iu.edu>
2021-04-28 20:24:32 -0400

    specialist trainer now uses yaml mapping files

--- train_specialist.py
+++ train_specialist.py
@@ -1,6 +1,8 @@
 import os
 import pathlib
+import shutil
 import json
+import yaml
 from typing import Optional, Sequence, Tuple, Union, List
 
 import numpy as np
@@ -19,24 +21,20 @@ import _datasets as D
 import _models as M
 
 ROOT = os.path.dirname(os.path.realpath(__file__))
-EPS = 1e-8
+DEFAULT_BATCH_SIZE = 128
 
 def ray_train_specialist(config, use_ray: bool = True):
 
-    # find specialist mapping
-    config['experiment_dir'] = str(list(
-        pathlib.Path('/media/sdb1/asivara/Research/2021_waspaa/weights_sv/'
-            ).glob('*hidden_size='+str(config['sv_hidden_size']) \
-                   +',utterance_duration=' \
-                   +str(config['utterance_duration'])+'*'))[0])
-
-    mapping_filepath = str(list(pathlib.Path(config['experiment_dir']
-        ).rglob('map_specialist_to_speakers.json'))[0])
+    # find specialist mapping file
+    mapping_filepath = os.path.join(
+        ROOT, 'weights',
+        'sv_mapping_K={:02d}.yaml'.format(config['num_clusters'])
+    )
     with open(mapping_filepath, 'r') as fp:
-        mapping = json.load(fp)
+        mapping = yaml.safe_load(fp)
 
-    # use the specialist mapping to determine the relevant speaker IDs
-    ### config['speaker_ids'] = mapping[str(config['specialist_index'])]
+    # grab a list of relevant speaker_ids
+    relevant_speakers = mapping['specialists'][config['specialist_index']]
 
     print(config)
 
@@ -49,25 +47,15 @@ def ray_train_specialist(config, use_ray: bool = True):
         device = 'cuda:0'
 
     network = M.NetSE(config['hidden_size'], config['num_layers']).to(device)
-    optimizer = torch.optim.Adam(network.parameters(), lr=config['learning_rate'])
-    # criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(network.parameters(),
+                                 lr=config['learning_rate'])
     criterion = LossSDR('sisdr')
 
-    data_tr = D.DatasetSE(
-        speaker_ids=mapping[str(config['specialist_index'])],
-        speech_subset='train',
-        noise_subset='train',
-        utterance_duration=config['utterance_duration'],
-        mixture_snr=config['mixing_snr'])
-    dl_tr = DataLoader(data_tr, batch_size=config['batch_size'])
+    data_tr = D.DatasetSE(relevant_speakers, 'train', 'train')
+    dl_tr = DataLoader(data_tr, batch_size=DEFAULT_BATCH_SIZE)
 
-    data_vl = D.DatasetSE(
-        speaker_ids=mapping[str(config['specialist_index'])],
-        speech_subset='val',
-        noise_subset='train',
-        utterance_duration=config['utterance_duration'],
-        mixture_snr=config['mixing_snr'])
-    (vx, vs, _) = next(iter(DataLoader(data_vl, batch_size=config['batch_size'])))
+    data_vl = D.DatasetSE(relevant_speakers, 'val', 'train')
+    (vx, vs, _) = next(iter(DataLoader(data_vl, batch_size=DEFAULT_BATCH_SIZE)))
     vx = vx.to(device)
     vs = vs.to(device)
 
@@ -83,7 +71,7 @@ def ray_train_specialist(config, use_ray: bool = True):
     for (x, s, n) in dl_tr:
 
         current_epoch += 1
-        num_batches += config['batch_size']
+        num_batches += len(x)
 
         optimizer.zero_grad()
         x = x.to(device)
@@ -91,8 +79,8 @@ def ray_train_specialist(config, use_ray: bool = True):
         y = network(x)
 
         min_len = min(y.shape[-1], s.shape[-1])
-        loss = criterion(y[..., :min_len], s[..., :min_len])
-        loss.mean().backward()
+        loss = criterion(y[..., :min_len], s[..., :min_len]).mean()
+        loss.backward()
         optimizer.step()
 
         # only validate every few epochs
@@ -116,7 +104,11 @@ def ray_train_specialist(config, use_ray: bool = True):
             best_sisdr = vl_sisdr
             best_state_dict = network.state_dict()
 
-        tune.report(num_batches=num_batches, vl_loss=best_result, vl_sisdr=best_sisdr)
+        tune.report(
+            num_batches=num_batches,
+            vl_loss=best_result,
+            vl_sisdr=best_sisdr
+        )
 
         # check for convergence
         if current_epoch - best_epoch > 200:
@@ -131,73 +123,79 @@ def ray_train_specialist(config, use_ray: bool = True):
                     path = os.path.join(checkpoint_dir, 'errors.json')
                     with open(path, 'w') as fp:
                         json.dump(errors, fp)
-                    path = os.path.join(checkpoint_dir, 'config.json')
-                    with open(path, 'w') as fp:
-                        json.dump(dict(
-                            experiment_dir=config['experiment_dir'],
-                            speaker_ids=mapping[str(config['specialist_index'])],
-                            mixing_snr=config['mixing_snr'],
-                            specialist_index=int(config['specialist_index']),
-                            utterance_duration=int(config['utterance_duration']),
-                            sv_hidden_size=int(config['sv_hidden_size']),
-                            hidden_size=int(config['hidden_size']),
-                            learning_rate=float(config['learning_rate']),
-                            num_layers=int(config['num_layers']),
-                            batch_size=int(config['batch_size']),
-                            ), fp)
             break
 
     print('exited train_specialist with args = {{' + \
         ', '.join([f'{k}={v}' for (k,v) in config.items()]) + '}}')
     return {
         # 'state_dict': best_state_dict,
-        'num_batches': best_epoch * config['batch_size'],
+        'num_batches': best_epoch * DEFAULT_BATCH_SIZE,
         'vl_loss': best_result,
         'vl_sisdr': best_sisdr,
     }
 
 
-def main(num_gpus: float = .25):
-    config = {
-        'mixing_snr': (-5, 10),
-        'specialist_index': tune.grid_search(list(np.arange(10))),
-        'utterance_duration': tune.grid_search([5, 3, 1]),
-        'sv_hidden_size': tune.grid_search([32, 64, 96]),
-        'hidden_size': 256,
-        'learning_rate': 1e-3, # tune.grid_search([1e-2, 1e-3, 1e-4]),
-        'num_layers': 2,
-        'batch_size': 128,
-    }
-    # scheduler = ASHAScheduler(
-    #     metric='vl_loss',
-    #     mode='min',
-    #     max_t=10000,
-    #     grace_period=100,
-    #     reduction_factor=2
-    # )
-    scheduler = None
-    result = tune.run(
-        ray_train_specialist,
-        config=config,
-        keep_checkpoints_num=1,
-        progress_reporter=CLIReporter(
-            max_report_frequency=60,
-            metric_columns=['num_batches', 'vl_sisdr'],
-            parameter_columns=['sv_hidden_size', 'utterance_duration', 'specialist_index'],
-        ),
-        resources_per_trial={'gpu': num_gpus},
-        verbose=1
-    )
-    best_trial = result.get_best_trial(
-        metric='vl_loss',
-        mode='min',
-        scope='last'
-    )
-    print('Best trial config: {}'.format(
-        best_trial.config))
-    print('Best trial final validation loss: {}'.format(
-        best_trial.last_result['vl_loss']))
+def main(num_gpus: float = .33, num_samples: int = 12):
 
+    # sweep hyperparameter space
+    for hidden_size in [64, 128, 256]:
+        for num_clusters in [2, 5, 10]:
+            for specialist_index in range(num_clusters):
+
+                output_dir = os.path.join(
+                    'weights', 'specialists',
+                    'se-hs={:04d}_K={:02d}_k={:02d}'.format(
+                    hidden_size, num_clusters, specialist_index))
+                pathlib.Path(output_dir).mkdir(0o777, True, True)
+
+                if os.path.exists(os.path.join(output_dir, 'checkpoint')):
+                    print(f'Skipping {output_dir}...')
+                    continue
+
+                config = {
+                    'hidden_size': hidden_size,
+                    'num_layers': 2,
+                    'mixing_snr': (-5, 10),
+                    'num_clusters': num_clusters,
+                    'specialist_index': specialist_index,
+                    'learning_rate': tune.grid_search(
+                        list(np.logspace(-4, -2, num_samples))),
+                }
+                result = tune.run(
+                    ray_train_specialist,
+                    config=config,
+                    keep_checkpoints_num=1,
+                    progress_reporter=CLIReporter(
+                        max_report_frequency=60,
+                        metric_columns=[
+                            'num_batches', 'vl_sisdr'
+                        ],
+                        parameter_columns=[
+                            'num_clusters', 'specialist_index',
+                            'hidden_size', 'learning_rate'
+                        ],
+                    ),
+                    resources_per_trial={'gpu': num_gpus},
+                    verbose=1
+                )
+                best_trial = result.get_best_trial(
+                    metric='vl_loss',
+                    mode='min',
+                    scope='last'
+                )
+                print('Best trial config: {}'.format(
+                    best_trial.config))
+                print('Best trial final validation loss: {}'.format(
+                    best_trial.last_result['vl_loss']))
+
+                f_src = os.path.join(best_trial.checkpoint.value, 'checkpoint')
+                print('Best trial checkpoint path: {}'.format(f_src))
+                f_dest = os.path.join(output_dir, 'checkpoint')
+                shutil.copy2(f_src, f_dest)
+                print('Copied best checkpoint to: {}'.format(f_dest))
+                f_dest = os.path.join(output_dir, 'config.json')
+                with open(f_dest, 'w') as fp:
+                    json.dump(best_trial.config, fp, indent=2)
 
 if __name__ == '__main__':
     main()
